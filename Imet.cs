585e64177dcc8bb640545a03f8810466183d1628
solnce1327@outlook.com <solnce1327@outlook.com>
2021-05-16 21:10:26 +0700

    Создан метод для возвращения строки

--- Imet.cs
+++ Imet.cs
@@ -8,6 +8,31 @@ namespace Солнце_Иван_1992с_Вариант_21
 {
     class Imet
     {
-
+        public static string GetScore(int K)
+        {
+            string Result = string.Empty;
+            switch (K)
+            {
+                case 1:
+                    Result = "Плохо";
+                    break;
+                case 2:
+                    Result = "Неудовлетворительно";
+                    break;
+                case 3:
+                    Result = "Удовлетворительно";
+                    break;
+                case 4:
+                    Result = "Хорошо";
+                    break;
+                case 5:
+                    Result = "Отлично";
+                    break;
+                default:
+                    Result = "Ошибка";
+                    break;
+            }
+            return Result;
+        }
     }
 }
