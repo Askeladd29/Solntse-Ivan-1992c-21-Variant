eb1c352a3083a1f47691ff6ff9a0d974c3e13967
solnce1327@outlook.com <solnce1327@outlook.com>
2021-05-16 21:05:03 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Солнце_Иван_1992с_Вариант_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
