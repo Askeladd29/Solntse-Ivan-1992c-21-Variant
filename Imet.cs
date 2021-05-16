52a89bf9e7ca9f413f2fe1df545ba371b4237e4f
solnce1327@outlook.com <solnce1327@outlook.com>
2021-05-16 21:07:49 +0700

    Добавим класс в котором будем описывать метод вывода строки

---
+++ Imet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Солнце_Иван_1992с_Вариант_21
+{
+    class Imet
+    {
+
+    }
+}
